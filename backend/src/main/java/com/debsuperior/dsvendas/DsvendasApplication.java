a2ff8a4b768186a79069e573934bed7bb91b3a68
Raphael Santiago <raphael.sant.emp@gmail.com>
2021-09-06 20:48:23 -0400

    Project created

---
+++ backend/src/main/java/com/debsuperior/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.debsuperior.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
