17217573d4cbbad621faf92035583943134207ad
Raphael Santiago <raphael.sant.emp@gmail.com>
2021-09-08 23:44:47 -0400

    Security config

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,4 +1,4 @@
-package com.debsuperior.dsvendas;
+package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
